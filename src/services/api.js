6b2746848cd6a2ad3733789208239c154b32228e
Ameena Begum <ameenabegum140@gmail.com>
2026-01-14 21:08:01 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "https://diagnostic-portal.onrender.com",
+    "https://diagnostic-portal.onrender.com/api",
   withCredentials: true,
 });
 
